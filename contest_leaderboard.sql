b9b7a5c1a7e64eeb111fe83d992ed36c857df2c1
RoshniSenapati03 <158549856+RoshniSenapati03@users.noreply.github.com>
2025-06-29 15:40:34 +0530

    Create contest_leaderboard.sql

---
+++ contest_leaderboard.sql
@@ -0,0 +1,1 @@
+SELECT h.hacker_id, h.name, SUM(ms.score) AS total_score FROM Hackers h JOIN (SELECT hacker_id, challenge_id, MAX(score) AS score FROM Submissions GROUP BY hacker_id, challenge_id) ms ON h.hacker_id = ms.hacker_id GROUP BY h.hacker_id, h.name HAVING total_score > 0 ORDER BY total_score DESC, h.hacker_id;
