2d6300f2da07b29ef01fd6ef29c9043c240da19d
RoshniSenapati03 <158549856+RoshniSenapati03@users.noreply.github.com>
2025-06-29 15:21:23 +0530

    Create top_competitors.sql

---
+++ top_competitors.sql
@@ -0,0 +1,1 @@
+SELECT h.hacker_id, h.name FROM Hackers h JOIN (SELECT s.hacker_id, COUNT(DISTINCT s.challenge_id) AS cnt FROM Submissions s JOIN Challenges c ON s.challenge_id = c.challenge_id JOIN Difficulty d ON c.difficulty_level = d.difficulty_level WHERE s.score = d.score GROUP BY s.hacker_id HAVING cnt > 1) t ON h.hacker_id = t.hacker_id ORDER BY t.cnt DESC, h.hacker_id;
