4e0f15af682c3abda59abf6201e8cb74cf91cfdb
RoshniSenapati03 <158549856+RoshniSenapati03@users.noreply.github.com>
2025-06-29 15:07:48 +0530

    Create weather_observation_station20.sql

---
+++ weather_observation_station20.sql
@@ -0,0 +1,1 @@
+SELECT CAST(AVG(LAT_N) AS DECIMAL(10,4)) FROM (SELECT LAT_N, ROW_NUMBER() OVER (ORDER BY LAT_N) AS rn, COUNT(*) OVER () AS total FROM STATION) AS temp WHERE rn = (total +1)/2 OR rn = (total + 2)/2; 
